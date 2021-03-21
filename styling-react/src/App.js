3a88cb2cbc2338a4b32284a780e56580182dd10a
Baruce <kimcj020828@gmail.com>
2021-03-21 23:13:57 +0900

    9.3 CSS Module까지 완료
    9.4styleed-componedts 부터 해야함

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import SassComponent from './SassComponent';
+// import SassComponent from './SassComponent';
+import CSSModule from './CSSModule';
 
 function App() {
   return (
     <div>
-      <SassComponent />
+      <CSSModule />
     </div>
   );
 }
+
 
 export default App;
